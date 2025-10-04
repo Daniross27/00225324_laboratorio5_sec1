cbb82b5d9edd22ae97e486febcb517fb0579a55d
Daniross27 <00225324@uca.edu.sv>
2025-10-04 16:35:54 -0600

    Feat:ejercios laboratorio 5

---
+++ EjercicioFinal/Data/Scientists.js
@@ -0,0 +1,19 @@
+import tesla from '../src/assets/tesla.jpg'
+import hawking from '../src/assets/hawking.jpg'
+
+export const Scientists = [
+{
+name: "Nikola Tesla",
+image: tesla,
+profession: "Inventor, ingeniero eléctrico, ingeniero mecánico y físico",
+awards: "Premio Edison, Medalla Elliott Cresson, Medalla John Scott",
+discoveries: "Corriente alterna, motor de inducción, bobina de Tesla"
+},
+{
+name: "Stephen Hawking",
+image: hawking,
+profession: "Físico teórico, astrofísico, cosmólogo y autor",
+awards: "Premio Eddington, Medalla Copley, Premio Fundamental Physics",
+discoveries: "Radiación de Hawking, Teoremas de singularidad, Teoría del Big Bang"
+}
+];
